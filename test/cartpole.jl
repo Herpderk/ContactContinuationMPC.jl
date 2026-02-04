e682994cf832751e97f301262038c156cf2707a4
Derek Fan <derekfan.18.2002@gmail.com>
2026-02-04 12:15:48 -0500

    remove allocations in cartpole costfunc

--- test/cartpole.jl
+++ test/cartpole.jl
@@ -63,16 +63,16 @@ Struct for storing cartpole system parameters and array caches.
 struct CartpoleSimulator{T<:AbstractFloat}
     dt::T
     dynamics::CartpoleDynamics{T}
-    x_tmp::DiffCache{Vector{T},Vector{T}}
+    xtmp::DiffCache{Vector{T},Vector{T}}
     xdot_tmp::DiffCache{Vector{T},Vector{T}}
 
     function CartpoleSimulator{T}(;
         mc::Real=1.0, mp::Real=0.1, l::Real=0.5, g::Real=9.81, dt::Real=0.05
     ) where {T<:AbstractFloat}
         dynamics = CartpoleDynamics{T}(; mc=mc, mp=mp, l=l, g=g)
-        x_tmp = DiffCache(zeros(T, dynamics.nx))
+        xtmp = DiffCache(zeros(T, dynamics.nx))
         xdot_tmp = DiffCache(zeros(T, dynamics.nx))
-        return new{T}(T(dt), dynamics, x_tmp, xdot_tmp)
+        return new{T}(T(dt), dynamics, xtmp, xdot_tmp)
     end
 end
 
@@ -91,7 +91,7 @@ Forward simulation function for the cartpole system. Computes the next state x1 
 
     # Get temporary state and dynamics vectors
     super_el = x0[1] + u0[1]
-    x_tmp = get_tmp(sim.x_tmp, super_el)
+    xtmp = get_tmp(sim.xtmp, super_el)
     xdot_tmp = get_tmp(sim.xdot_tmp, super_el)
 
     # RK4-integrate in place
@@ -103,18 +103,18 @@ Forward simulation function for the cartpole system. Computes the next state x1 
     @. x1 += dt/6 * xdot_tmp
 
     # Compute k2
-    @. x_tmp = x0 + dt/2 * xdot_tmp
-    sim.dynamics(xdot_tmp, x_tmp, u0)
+    @. xtmp = x0 + dt/2 * xdot_tmp
+    sim.dynamics(xdot_tmp, xtmp, u0)
     @. x1 += dt/3 * xdot_tmp
 
     # Compute k3
-    @. x_tmp = x0 + dt/2 * xdot_tmp
-    sim.dynamics(xdot_tmp, x_tmp, u0)
+    @. xtmp = x0 + dt/2 * xdot_tmp
+    sim.dynamics(xdot_tmp, xtmp, u0)
     @. x1 += dt/3 * xdot_tmp
 
     # Compute k4
-    @. x_tmp = x0 + dt * xdot_tmp
-    sim.dynamics(xdot_tmp, x_tmp, u0)
+    @. xtmp = x0 + dt * xdot_tmp
+    sim.dynamics(xdot_tmp, xtmp, u0)
     @. x1 += dt/6 * xdot_tmp
     return nothing
 end
@@ -125,7 +125,11 @@ end
 Backward differentiation function for the cartpole system. Computes the Jacobians A and B of the next state.
 """
 function (sim::CartpoleSimulator{T})(
-    A::Matrix{T}, B::VecOrMat{T}, x1::Vector{T}, x0::Vector{T}, u0::Vector{T}
+    A::AbstractMatrix{T},
+    B::AbstractMatrix{T},
+    x1::AbstractVector{<:Real},
+    x0::AbstractVector{<:Real},
+    u0::AbstractVector{<:Real},
 )::Nothing where {T}
     ForwardDiff.jacobian!(A, (δx1, δx0) -> sim(δx1, δx0, u0), x1, x0)
     ForwardDiff.jacobian!(B, (δx1, δu0) -> sim(δx1, x0, δu0), x1, u0)
@@ -141,36 +145,48 @@ struct QuadraticCostFunction{T<:AbstractFloat}
     Q::Matrix{T}
     R::Matrix{T}
     Qf::Matrix{T}
+    xtmp::DiffCache{Vector{T},Vector{T}}
+    utmp::DiffCache{Vector{T},Vector{T}}
 
     function QuadraticCostFunction{T}(
         Q::AbstractMatrix{<:Real},
         R::AbstractMatrix{<:Real},
         Qf::AbstractMatrix{<:Real},
     ) where {T<:AbstractFloat}
-        return new{T}(T.(Q), T.(R), T.(Qf))
+        xtmp = DiffCache(zeros(T, size(Q)[1]))
+        utmp = DiffCache(zeros(T, size(R)[1]))
+        return new{T}(T.(Q), T.(R), T.(Qf), xtmp, utmp)
     end
 end
 
 """
-    (weights::QuadraticCostFunction)(xerr, uerr)
+    (cache::QuadraticCostFunction)(xerr, uerr)
 
 Stage cost function for a quadratic cost. Computes the cost given the state error xerr and control error uerr.
 """
-function (weights::QuadraticCostFunction{T})(
+function (cache::QuadraticCostFunction{T})(
     xerr::AbstractVector{<:Real}, uerr::AbstractVector{<:Real}
 )::Union{T,ForwardDiff.Dual} where {T}
-    return 0.5 * (xerr' * weights.Q * xerr + uerr' * weights.R * uerr)
+    super_el = xerr[1] + uerr[1]
+    xtmp = get_tmp(cache.xtmp, super_el)
+    utmp = get_tmp(cache.utmp, super_el)
+
+    mul!(xtmp, cache.Q, xerr)
+    mul!(utmp, cache.R, uerr)
+    return 0.5 * (dot(xerr, xtmp) + dot(uerr, utmp))
 end
 
 """
-    (weights::QuadraticCostFunction)(xerr)
+    (cache::QuadraticCostFunction)(xerr)
 
 Terminal cost function for a quadratic cost. Computes the cost given the state error xerr.
 """
-function (weights::QuadraticCostFunction{T})(
+function (cache::QuadraticCostFunction{T})(
     xerr::AbstractVector{<:Real}
 )::Union{T,ForwardDiff.Dual} where {T}
-    return 0.5 * xerr' * weights.Qf * xerr
+    xtmp = get_tmp(cache.xtmp, xerr)
+    mul!(xtmp, cache.Qf, xerr)
+    return 0.5 * dot(xerr, xtmp)
 end
 
 function clean_solve(
