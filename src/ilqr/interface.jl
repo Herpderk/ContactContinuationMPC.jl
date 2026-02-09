9642eebc2fc82724780cc6d64cc1355b305c869f
Derek Fan <derekfan.18.2002@gmail.com>
2026-02-08 20:17:34 -0500

    convert outer interface constructors to inner

--- src/ilqr/interface.jl
+++ src/ilqr/interface.jl
@@ -115,44 +115,40 @@ mutable struct TrajoptSolution{T<:AbstractFloat}
     U::Vector{Vector{T}}
     J::T
     is_optimal::Bool
-end
 
-function TrajoptSolution(
-    params::TrajoptParameters{T,Lk,Lf}
-)::TrajoptSolution{T} where {T,Lk,Lf}
-    # Get problem dims
-    nx = get_nx(params.mfwd)
-    nu = params.mfwd.nu
-    N = length(params.Xref)
+    function TrajoptSolution(params::TrajoptParameters{T,Lk,Lf}) where {T,Lk,Lf}
+        # Get problem dims
+        nx = get_nx(params.mfwd)
+        nu = params.mfwd.nu
+        N = length(params.Xref)
 
-    # Initialize solution terms from dims
-    X = [zeros(T, nx) for k in 1:N]
-    U = [zeros(T, nu) for k in 1:(N - 1)]
-    J = zero(T)
-    is_optimal = false
-    return TrajoptSolution{T}(X, U, J, is_optimal)
+        # Initialize solution terms from dims
+        X = [zeros(T, nx) for k in 1:N]
+        U = [zeros(T, nu) for k in 1:(N - 1)]
+        J = zero(T)
+        is_optimal = false
+        return new{T}(X, U, J, is_optimal)
+    end
 end
 
 mutable struct ILqrCache{T<:AbstractFloat}
     fwd::ForwardCache{T}
     bwd::BackwardCache{T}
     tmp::TemporaryCache{T}
-end
 
-function ILqrCache(
-    params::TrajoptParameters{T,Lk,Lf}
-)::ILqrCache{T} where {T,Lk,Lf}
-    # Get problem dims
-    nx = get_nx(params.mfwd)
-    ndx = get_ndx(params.mfwd)
-    nu = params.mfwd.nu
-    N = length(params.Xref)
+    function ILqrCache(params::TrajoptParameters{T,Lk,Lf}) where {T,Lk,Lf}
+        # Get problem dims
+        nx = get_nx(params.mfwd)
+        ndx = get_ndx(params.mfwd)
+        nu = params.mfwd.nu
+        N = length(params.Xref)
 
-    # Initialize caches from dims
-    fwd = ForwardCache{T}(nx, nu, N)
-    bwd = BackwardCache{T}(ndx, nu, N)
-    tmp = TemporaryCache{T}(nx, ndx, nu)
-    return ILqrCache{T}(fwd, bwd, tmp)
+        # Initialize caches from dims
+        fwd = ForwardCache{T}(nx, nu, N)
+        bwd = BackwardCache{T}(ndx, nu, N)
+        tmp = TemporaryCache{T}(nx, ndx, nu)
+        return new{T}(fwd, bwd, tmp)
+    end
 end
 
 @option struct DefaultILqrOptions{T<:AbstractFloat}
@@ -175,44 +171,45 @@ mutable struct ILqrOptions{T<:AbstractFloat}
     maxiter_ilqr::Int
     maxiter_ls::Int
     is_verbose::Bool
-end
 
-function ILqrOptions{T}(;
-    alpha_mul::Union{<:AbstractFloat,Nothing}=nothing,
-    margin_ls::Union{<:AbstractFloat,Nothing}=nothing,
-    eps_reg::Union{<:AbstractFloat,Nothing}=nothing,
-    eps_fd::Union{<:AbstractFloat,Nothing}=nothing,
-    tol_converge::Union{<:AbstractFloat,Nothing}=nothing,
-    maxiter_ilqr::Union{Int,Nothing}=nothing,
-    maxiter_ls::Union{Int,Nothing}=nothing,
-    is_verbose::Union{Bool,Nothing}=nothing,
-)::ILqrOptions{T} where {T}
-    # Load default options from config
-    default = from_toml(
-        DefaultILqrOptions{T}, joinpath(@__DIR__, "config/default_opts.toml")
-    )
+    function ILqrOptions{T}(;
+        alpha_mul::Union{<:AbstractFloat,Nothing}=nothing,
+        margin_ls::Union{<:AbstractFloat,Nothing}=nothing,
+        eps_reg::Union{<:AbstractFloat,Nothing}=nothing,
+        eps_fd::Union{<:AbstractFloat,Nothing}=nothing,
+        tol_converge::Union{<:AbstractFloat,Nothing}=nothing,
+        maxiter_ilqr::Union{Int,Nothing}=nothing,
+        maxiter_ls::Union{Int,Nothing}=nothing,
+        is_verbose::Union{Bool,Nothing}=nothing,
+    )::ILqrOptions{T} where {T}
+        # Load default options from config
+        default = from_toml(
+            DefaultILqrOptions{T},
+            joinpath(@__DIR__, "config/default_opts.toml"),
+        )
 
-    # Use default options if the corresponding option is nothing
-    alpha_mul_ = isnothing(alpha_mul) ? default.alpha_mul : T(alpha_mul)
-    margin_ls_ = isnothing(margin_ls) ? default.margin_ls : T(margin_ls)
-    eps_reg_ = isnothing(eps_reg) ? default.eps_reg : T(eps_reg)
-    eps_fd_ = isnothing(eps_fd) ? default.eps_fd : T(eps_fd)
-    tol_converge_ =
-        isnothing(tol_converge) ? default.tol_converge : T(tol_converge)
-    maxiter_ilqr_ =
-        isnothing(maxiter_ilqr) ? default.maxiter_ilqr : maxiter_ilqr
-    maxiter_ls_ = isnothing(maxiter_ls) ? default.maxiter_ls : maxiter_ls
-    is_verbose_ = isnothing(is_verbose) ? default.is_verbose : is_verbose
-    return ILqrOptions{T}(
-        alpha_mul_,
-        margin_ls_,
-        eps_reg_,
-        eps_fd_,
-        tol_converge_,
-        maxiter_ilqr_,
-        maxiter_ls_,
-        is_verbose_,
-    )
+        # Use default options if the corresponding option is nothing
+        alpha_mul_ = isnothing(alpha_mul) ? default.alpha_mul : T(alpha_mul)
+        margin_ls_ = isnothing(margin_ls) ? default.margin_ls : T(margin_ls)
+        eps_reg_ = isnothing(eps_reg) ? default.eps_reg : T(eps_reg)
+        eps_fd_ = isnothing(eps_fd) ? default.eps_fd : T(eps_fd)
+        tol_converge_ =
+            isnothing(tol_converge) ? default.tol_converge : T(tol_converge)
+        maxiter_ilqr_ =
+            isnothing(maxiter_ilqr) ? default.maxiter_ilqr : maxiter_ilqr
+        maxiter_ls_ = isnothing(maxiter_ls) ? default.maxiter_ls : maxiter_ls
+        is_verbose_ = isnothing(is_verbose) ? default.is_verbose : is_verbose
+        return new{T}(
+            alpha_mul_,
+            margin_ls_,
+            eps_reg_,
+            eps_fd_,
+            tol_converge_,
+            maxiter_ilqr_,
+            maxiter_ls_,
+            is_verbose_,
+        )
+    end
 end
 
 # Default type parameter
