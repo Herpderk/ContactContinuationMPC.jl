2e75b0c9e90f302c2aeca2f570b95a05eadd1e67
Derek Fan <derekfan.18.2002@gmail.com>
2026-02-13 03:51:32 -0500

    fix option assertions

--- src/ilqr/solve.jl
+++ src/ilqr/solve.jl
@@ -47,37 +47,43 @@ function assert_opts!(opts::ILqrOptions)::Nothing
             "The backtracking contraction rate must be between 0 and 1",
         )
     end
-    if opts.eps_reg < 0.0
+    if opts.eps_reg <= 0.0
         throwdom(
             opts.eps_reg,
             "The regularizer coefficient must be greater than or equal to 0",
         )
     end
-    if opts.eps_fd <= 0.0
+    if opts.eps_fd < 0.0
         throwdom(
             opts.eps_fd,
             "The finite-difference coefficient must be greater than 0",
         )
     end
-    if opts.tol_converge <= 0.0
+    if opts.tol_interp < 0.0
+        throwdom(
+            opts.tol_interp,
+            "The dynamics jacobian interpolation tolerance must be greater than 0",
+        )
+    end
+    if opts.tol_converge < 0.0
         throwdom(
             opts.tol_converge,
             "The stationarity tolerance must be greater than 0",
         )
     end
-    if opts.margin_ls < 0.0
+    if opts.margin_ls <= 0.0
         throwdom(
             opts.margin_ls,
             "The merit function margin factor must be greater than or equal to 0",
         )
     end
-    if opts.maxiter_ilqr <= 0
+    if opts.maxiter_ilqr < 0
         throwdom(
             opts.maxiter_ilqr,
             "The max number of iterations must be greater than 0",
         )
     end
-    if opts.maxiter_ls <= 0
+    if opts.maxiter_ls < 0
         throwdom(
             opts.maxiter_ls,
             "The max number of line-search iterations must be greater than 0",
