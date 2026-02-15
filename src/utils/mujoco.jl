817abc7ad300336268d7a06ed76f7536ebbbe165
Derek Fan <derekfan.18.2002@gmail.com>
2026-02-15 01:37:10 -0500

    add go2 falling script

--- src/utils/mujoco.jl
+++ src/utils/mujoco.jl
@@ -165,12 +165,7 @@ function get_joint_names(m::Model)::Vector{String}
     names = ["" for i in 1:m.njnt]
     for i in 1:m.njnt
         ptr = mj_id2name(m, MuJoCo.mjOBJ_JOINT, i-1)
-        if ptr != C_NULL
-            name = unsafe_string(ptr)
-        else
-            name = "unnamed"
-        end
-        names[i] = name
+        names[i] = ptr == C_NULL ? "unnamed" : unsafe_string(ptr)
     end
     return names
 end
@@ -179,12 +174,7 @@ function get_geom_names(m::Model)::Vector{String}
     names = ["" for i in 1:m.ngeom]
     for i in 1:m.ngeom
         ptr = mj_id2name(m, MuJoCo.mjOBJ_GEOM, i-1)
-        if ptr != C_NULL
-            name = unsafe_string(ptr)
-        else
-            name = "unnamed"
-        end
-        names[i] = name
+        names[i] = ptr == C_NULL ? "unnamed" : unsafe_string(ptr)
     end
     return names
 end
