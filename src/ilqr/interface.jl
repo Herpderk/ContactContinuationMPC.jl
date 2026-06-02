a13dba27f6f592d072a3b403ed6d26a0610f745c
Derek Fan <derekfan.18.2002@gmail.com>
2026-06-02 01:03:37 -0700

    automate iLQROption definition based on default definition

--- src/ilqr/interface.jl
+++ src/ilqr/interface.jl
@@ -37,77 +37,29 @@ end
     save_bestsol::Bool
 end
 
-mutable struct iLQROptions{T<:AbstractFloat}
-    rho_init::T
-    rho_mul::T
-    alpha_mul::T
-    margin_ls::T
-    eps_reg::T
-    eps_fd::T
-    tol_interp::T
-    tol_constr::T
-    tol_ilqr::T
-    maxiter_al::Int
-    maxiter_ilqr::Int
-    maxiter_ls::Int
-    is_verbose::Bool
-    save_bestsol::Bool
+@composite mutable struct iLQROptions{T<:AbstractFloat}
+    DefaultiLQROptions{T}...
 
-    function iLQROptions{T}(;
-        rho_init::Union{<:AbstractFloat,Nothing}=nothing,
-        rho_mul::Union{<:AbstractFloat,Nothing}=nothing,
-        alpha_mul::Union{<:AbstractFloat,Nothing}=nothing,
-        margin_ls::Union{<:AbstractFloat,Nothing}=nothing,
-        eps_reg::Union{<:AbstractFloat,Nothing}=nothing,
-        eps_fd::Union{<:AbstractFloat,Nothing}=nothing,
-        tol_interp::Union{<:AbstractFloat,Nothing}=nothing,
-        tol_constr::Union{<:AbstractFloat,Nothing}=nothing,
-        tol_ilqr::Union{<:AbstractFloat,Nothing}=nothing,
-        maxiter_al::Union{Int,Nothing}=nothing,
-        maxiter_ilqr::Union{Int,Nothing}=nothing,
-        maxiter_ls::Union{Int,Nothing}=nothing,
-        is_verbose::Union{Bool,Nothing}=nothing,
-        save_bestsol::Union{Bool,Nothing}=nothing,
-    ) where {T}
+    function iLQROptions(; kwargs...)
         # Load default options from config
         default = from_toml(
             DefaultiLQROptions{T},
             joinpath(@__DIR__, "config/default_opts.toml"),
         )
 
-        # Use default options if the corresponding option is nothing
-        rho_init_ = isnothing(rho_init) ? default.rho_init : T(rho_init)
-        rho_mul_ = isnothing(rho_mul) ? default.rho_mul : T(rho_mul)
-        alpha_mul_ = isnothing(alpha_mul) ? default.alpha_mul : T(alpha_mul)
-        margin_ls_ = isnothing(margin_ls) ? default.margin_ls : T(margin_ls)
-        eps_reg_ = isnothing(eps_reg) ? default.eps_reg : T(eps_reg)
-        eps_fd_ = isnothing(eps_fd) ? default.eps_fd : T(eps_fd)
-        tol_interp_ = isnothing(tol_interp) ? default.tol_interp : T(tol_interp)
-        tol_constr_ = isnothing(tol_constr) ? default.tol_constr : T(tol_constr)
-        tol_ilqr_ = isnothing(tol_ilqr) ? default.tol_ilqr : T(tol_ilqr)
-        maxiter_al_ = isnothing(maxiter_al) ? default.maxiter_al : maxiter_al
-        maxiter_ilqr_ =
-            isnothing(maxiter_ilqr) ? default.maxiter_ilqr : maxiter_ilqr
-        maxiter_ls_ = isnothing(maxiter_ls) ? default.maxiter_ls : maxiter_ls
-        is_verbose_ = isnothing(is_verbose) ? default.is_verbose : is_verbose
-        save_bestsol_ =
-            isnothing(save_bestsol) ? default.save_bestsol : save_bestsol
-        return new{T}(
-            rho_init_,
-            rho_mul_,
-            alpha_mul_,
-            margin_ls_,
-            eps_reg_,
-            eps_fd_,
-            tol_interp_,
-            tol_constr_,
-            tol_ilqr_,
-            maxiter_al_,
-            maxiter_ilqr_,
-            maxiter_ls_,
-            is_verbose_,
-            save_bestsol_,
+        macro symbolof(var)
+            return QuoteNode(var)
+        end
+        function default_opt(opt)
+            return getfield(default, @symbolof(opt))
+        end
+
+        # Get the names of the fields in the struct
+        fnames = fieldnames(DefaultiLQROptions{T})
+        fvals = Tuple(
+            get(kwargs, fname, default_opt(fname)) for fname in fnames
         )
+        return new{T}(fvals...)
     end
 end
 
